194e2b54befe656bd8871746348998d41e9f254b
rifedothman <rifed.othmen@gmail.com>
2025-02-13 13:47:49 +0100

    user,message SQL namedQueries

---
+++ whatsappclone/src/main/java/com/rifed/whatsappclone/user/UserConstants.java
@@ -0,0 +1,10 @@
+package com.rifed.whatsappclone.user;
+
+public class UserConstants {
+
+    public static final String FIND_USER_BY_EMAIL = "User.findUserByEmail";
+    public static final String FIND_ALL_USERS_EXCEPT_SELF = "User.findAllUsersExceptSelf";
+    public static final String FIND_USER_BY_PUBLIC_ID = "User.findUserByPublicId";
+
+    private UserConstants() {}
+}
